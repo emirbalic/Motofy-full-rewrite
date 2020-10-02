d2c1b602784c2f5c97c69031e238414c7e5b7f2c
Emir Balic <emirbalic@gmail.com>
2020-10-02 14:19:26 +0200

    Identity

--- API/Controllers/BrandsController.cs
+++ API/Controllers/BrandsController.cs
@@ -9,26 +9,26 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BrandsController : ControllerBase
+    // [Route("api/[controller]")]
+    // [ApiController]
+    public class BrandsController : BaseController
     {
-        private readonly IMediator _mediator;
-        public BrandsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        // private readonly IMediator _mediator;
+        // public BrandsController(IMediator mediator)
+        // {
+        //     _mediator = mediator;
+        // }
 
         [HttpGet]
         public async Task<ActionResult<List<Brand>>> List()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Brand>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query { Id = id });
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,24 @@
 // using Application.Activities;
 // using Application.Brands;
+using System.Text;
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
+using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
 namespace API
@@ -30,7 +39,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-            services.AddControllers()
+            services.AddControllers(opt => 
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
                 .AddFluentValidation(cfg => 
                 {
                     // here might be necessery to specify all folders
@@ -46,6 +60,28 @@ namespace API
             // services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddMediatR(typeof(Application.Activities.List.Handler).Assembly);
             services.AddMediatR(typeof(Application.Brands.List.Handler).Assembly);
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt => 
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = key,
+                      ValidateAudience = false,
+                      ValidateIssuer = false
+                    };
+                });
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,9 +98,11 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
